61d5b01fa6ba07e36891ea4a872919446306bd13
lmwm <61632595+lmwm@users.noreply.github.com>
2026-09-25 17:19:42 +0800

    更新 Packages.sh

--- Devices/Cudy TR3000/Packages.sh
+++ Devices/Cudy TR3000/Packages.sh
@@ -15,28 +15,28 @@ OPENWRT_DIR="$(pwd)"
 # luci-theme-argon (最新版)
 # 删除 feeds 旧版本，克隆到 package/app/
 # -----------------------------------------------------------
-find "$OPENWRT_DIR/package/feeds/" -name "luci-theme-argon" -exec rm -rf {} + 2>/dev/null
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git \
-    "$OPENWRT_DIR/package/app/luci-theme-argon"
-echo "[OK] 已添加: luci-theme-argon"
+#find "$OPENWRT_DIR/package/feeds/" -name "luci-theme-argon" -exec rm -rf {} + 2>/dev/null
+#git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git \
+#    "$OPENWRT_DIR/package/app/luci-theme-argon"
+#echo "[OK] 已添加: luci-theme-argon"
 
 # -----------------------------------------------------------
 # luci-app-argon-config (配套)
 # 删除 feeds 旧版本，克隆到 package/app/
 # -----------------------------------------------------------
-find "$OPENWRT_DIR/package/feeds/" -name "luci-app-argon-config" -exec rm -rf {} + 2>/dev/null
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git \
-    "$OPENWRT_DIR/package/app/luci-app-argon-config"
-echo "[OK] 已添加: luci-app-argon-config"
+#find "$OPENWRT_DIR/package/feeds/" -name "luci-app-argon-config" -exec rm -rf {} + 2>/dev/null
+#git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git \
+#    "$OPENWRT_DIR/package/app/luci-app-argon-config"
+#echo "[OK] 已添加: luci-app-argon-config"
 
 # -----------------------------------------------------------
 # OpenClash
 # 删除 feeds 旧版本，克隆到 package/app/
 # -----------------------------------------------------------
-find "$OPENWRT_DIR/package/feeds/" -name "luci-app-openclash" -exec rm -rf {} + 2>/dev/null
-git clone --depth 1 https://github.com/vernesong/OpenClash.git \
-    "$OPENWRT_DIR/package/app/OpenClash"
-echo "[OK] 已添加: OpenClash (luci-app-openclash)"
+#find "$OPENWRT_DIR/package/feeds/" -name "luci-app-openclash" -exec rm -rf {} + 2>/dev/null
+#git clone --depth 1 https://github.com/vernesong/OpenClash.git \
+#    "$OPENWRT_DIR/package/app/OpenClash"
+#echo "[OK] 已添加: OpenClash (luci-app-openclash)"
 
 # -----------------------------------------------------------
 # （已移除）OpenAppFilter
